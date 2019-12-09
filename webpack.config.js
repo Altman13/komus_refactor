0984d98f31aa0a2c6dd8fc2a17aee704a8489396
hromov.ga <consent71@gmail.com>
2019-12-09 15:29:13 +0300

    webpack+react ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require("path");
 module.exports = {
-  entry: path.join(__dirname, "src", "app.js"),
-  output: { path: path.join(__dirname, "src", "dist"), filename: "index.js" },
-  module: {
-    rules: [
-      { test: /\.js$/, loader: "babel-loader", exclude: "/node_modules/" }
-    ]
+  entry: path.join(__dirname, "./src", "index.js"),
+  output: {
+    path: path.join(__dirname, "./src", "./../dist"),
+    filename: "index.js"
   },
-  devServer:{
-      overlay: true
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
   }
 };
