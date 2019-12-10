9644486e438e6eeab2c218b8bd9f47a5ffd8226e
hromov.ga <consent71@gmail.com>
2019-12-10 10:16:02 +0300

    webpack hmr add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+
 class HelloWorld extends React.Component {
-    render() {
-          return (
-                  <div>
-                    Hello, React!
-                  </div>
-                )
-        }
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos/10')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
+
+  render() {
+    return ( <div>
+      Hello, React!
+      </div>
+    )
+  }
 };
- 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+
+ReactDOM.render( < HelloWorld / > , document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
   entry: path.join(__dirname, "./src", "index.js"),
   output: {
-    path: path.join(__dirname, "./src", "./../dist"),
+    //path: path.join(__dirname, "./src", "./../dist"),
+    //filename: "index.js"
+    path: path.join(__dirname, "./src", "./../public"),
     filename: "index.js"
   },
   module: {
@@ -12,5 +18,18 @@ module.exports = {
         use: ['babel-loader']
       }
     ]
-  }
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: __dirname + "/src/",
+    hot: true,
+  },
+  plugins: [
+    
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
+      template: './src/index.html'
+    }),
+  ],
 };
