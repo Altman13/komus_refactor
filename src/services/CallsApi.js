8778958702b5ae0ca1b559a09f694e435a8788cd
Altman13 <consent71@gmail.com>
2020-01-02 11:24:02 +0300

    add api services

---
+++ src/services/CallsApi.js
@@ -0,0 +1,12 @@
+import ApiService from './Api';
+
+const JSON_QUERY = '.json?print=pretty';
+const BASE_URL = '/react/php/komus_new/test.php';
+const client = new ApiService({ baseURL: BASE_URL });
+
+const CallsApi = {};
+
+CallsApi.getCalls = () => client.get(`/calls${JSON_QUERY}`);
+CallsApi.getCall = id => client.get(`/calls/${id}${JSON_QUERY}`);
+
+export default CallsApi;
