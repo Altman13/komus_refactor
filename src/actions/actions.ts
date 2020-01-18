daaf01151a06fd15ee7e86f523e6ca9604bdf598
Altman13 <consent71@gmail.com>
2020-01-18 11:14:13 +0300

    fetch-ok, make_action-ok, disptach-not ok

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,48 +1,26 @@
-import { Contact } from './../models/contact';
-import { MAKE_CALL, RECEIVE_CALL } from './../models/actions';
-// import { CallActionTypes } from "./../models/actions";
+import { Contact } from "./../models/contact"
+import { MAKE_CALL, RECEIVE_CALL } from "./../models/actions"
+import actionCreatorFactory from "typescript-fsa"
+import { asyncFactory } from "typescript-fsa-redux-thunk"
+import { AppState } from "../store"
 
-// import { RECEIVE_CALL, MAKE_CALL } from "../models/actions";
-// export const makeCall = (contact: Contact): CallActionTypes => ({
-//   type: MAKE_CALL,
-//   contact
-// });
-// export const receiveCall = (contact: Contact): CallActionTypes => ({
-//   type: RECEIVE_CALL,
-//   contact
-// });
-import actionCreatorFactory from "typescript-fsa";
-import { asyncFactory } from "typescript-fsa-redux-thunk";
-import { AppState } from "../store";
-
-const create = actionCreatorFactory(MAKE_CALL);
-const createAsync = asyncFactory<AppState>(create);
-
-const call_make = create<Contact>(MAKE_CALL);
+const create = actionCreatorFactory(MAKE_CALL)
+const createAsync = asyncFactory<AppState>(create)
+const call_make = create<Contact>(MAKE_CALL)
 
 export const rec_call = createAsync<any, any>(
   "CALL_MAKE",
   async (params, dispatch) => {
-    const url = `https://jsonplaceholder.typicode.com/users`;
-    const options: RequestInit = {
-      method: "POST",
-      body: JSON.stringify(params),
-      headers: {
-        "Content-Type": "application/json; charset=utf-8"
+    try {
+      let response = await fetch("https://jsonplaceholder.typicode.com/users")
+      if (!response.ok) {
+        throw new Error(response.statusText)
       }
-    };
-    const res = await fetch(url, options);
-    if (!res.ok) {
-      throw new Error(`Error ${res.status}: ${res.statusText}`);
+      let body = await response.json()
+      console.table(body)
+      return dispatch(call_make)
+    } catch (err) {
+      console.log(err)
     }
-    dispatch(call_make);
-    return res.json();
   }
-);
-
-// import { createAsyncAction } from 'redux-promise-middleware-actions';
-
-// export const fetchData = createAsyncAction('FETCH_DATA', async () => {
-//   const res = await fetch(`https://reqres.in/api/login`);
-//   return res.json();
-// });
+)
