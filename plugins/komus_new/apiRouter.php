f81e0918f21539e0b0a5748f7d6f5f50a4f946c4
hromov.ga <consent71@gmail.com>
2020-02-06 16:50:12 +0300

    small edits

--- plugins/komus_new/apiRouter.php
+++ plugins/komus_new/apiRouter.php
@@ -5,14 +5,17 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require "config/config.php";
 require 'vendor/autoload.php';
-//require 'controllers/callController.php';
-require 'vendor/autoload.php';
 require 'controllers/HomeController.php';
 require 'controllers/ReportsController.php';
 require 'controllers/CallsController.php.php';
 
-
 $app = new \Slim\App;
+
+// $app->add(new Tuupola\Middleware\JwtAuthentication([
+//      "path" => "/api", /* or ["/api", "/admin"] */
+//     "secret" => getenv("JWT_SECRET")
+// ]));
+
 $app->get('/api/home', HomeController::class . ':home');
 $app->get('/api/calls', CallsController::class . ':show');
 $app->post('/api/calls', CallsController::class . ':create');
@@ -25,12 +28,9 @@ $app->post('/api/mail', MailController::class . ':send');
 $app->post('/api/user', UsersController::class . ':create');
 $app->get('/api/user', UsersController::class . ':show');
 //Get xml, json, html, 
-$app->
-$app->get('/api/entrypoint', EntryPointController::class . ':show');
+$app->$app->get('/api/entrypoint', EntryPointController::class . ':show');
 //Post xml, json, formdata
 $app->post('/api/entrypoint', EntryPointController::class . ':create');
-
-
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
@@ -38,47 +38,3 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $newResponse;
 });
 $app->run();
-
-//$app = new Slim\App;
-//$router = new \Bramus\Router\Router();
-
-// $app->add(new Tuupola\Middleware\JwtAuthentication([
-//      "path" => "/api", /* or ["/api", "/admin"] */
-//     "secret" => getenv("JWT_SECRET")
-// ]));
-//$router->get('/calls/', '\Controllers\callController');
-// $router->get('/calls/', function() {
-  
-// });
-
-// $router->get('/api/report', function() {
-//     echo 'get report';
-// }); 
-// $router->get('/api/report/period', function($date_begin, $date_end) {
-//     echo 'get report period';
-// });
-// $router->get('/api/report/operator/'{id}, function($id) {
-//     echo 'get report operator';
-// });
-// $router->get('/api/contacts', function() {
-//     echo 'get contacts';
-// });
-// $router->post('/api/contacts', function($request) {
-//     echo 'post contacts';
-// });
-// $router->get('/api/filter/recall', function() {
-//     echo 'get contacts filter recall';
-// });
-// $router->get('/api/handbook', function() {
-//     echo 'get handbook';
-// });
-// $router->get('/api/search', function() {
-//     echo 'get search';
-// });
-// $router->get('/api/entrypoint', function() {
-//     echo 'get enterypoint';
-// });
-// $router->post('/api/entrypoint', function($request) {
-//     echo 'post enterypoint';
-// });
-//$router->run();
