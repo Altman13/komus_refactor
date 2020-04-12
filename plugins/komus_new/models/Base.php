ab95ead9e46d1208117329f4661acf526c1c2d3c
Altman13 <consent71@gmail.com>
2020-04-12 16:28:30 +0300

    baseController small edits

--- plugins/komus_new/models/Base.php
+++ plugins/komus_new/models/Base.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Komus;
-//header("Access-Control-Allow-Origin: *");
+
 use Slim\Http\UploadedFile;
 
 class Base
@@ -50,22 +50,23 @@ class Base
             $s = str_replace(" ", "-", $s); // заменяем пробелы знаком минус
             return $s; // возвращаем результат
         }
-        $for_sortable = array();
         //TODO : убедиться в том, что не может быть пропущенных пустых столбцов в файле для импорта базы,
         //иначе загрузка произойдет до первого пустого столбца заголовка
         //номер строки в excel файле 
+        $for_sortable = array();
+        $str_q='("INSERT INTO contacts (';
         for ($i = 0;; $i++) {
             $column_name = $obj_php_excel->getActiveSheet()->getCellByColumnAndRow($i, 1)->getValue();
             if ($column_name != NULL) {
                 $column_name_translit = translit($column_name);
                 $one_word_column_name = explode('-', $column_name_translit, 2);
                 $one_word_column_name = preg_replace("/[^a-zA-ZА\s]/", '', $one_word_column_name[0]);
+                array_push($for_sortable, $one_word_column_name);
+                $str_q.='`'.$one_word_column_name.'`, ';
             } else {
                 break;
             }
-            // ALTER TABLE `contacts` ADD COLUMN 
-            // test VARCHAR(255) NULL DEFAULT NULL AFTER `id`  
-            $alter_table_contacts = $this->db->prepare("ALTER TABLE contacts ADD $one_word_column_name VARCHAR(255)");
+            $alter_table_contacts = $this->db->prepare("ALTER TABLE contacts ADD IF NOT EXISTS $one_word_column_name VARCHAR(255)");
             try {
                 $alter_table_contacts->execute();
             } catch (\Throwable $th) {
@@ -75,64 +76,35 @@ class Base
                     ' по адресу: ' . $_SERVER['SCRIPT_NAME']);
             }
         }
-        array_push($for_sortable, $one_word_column_name);
-        //}
-        // global $columns_for_insert;
-        // foreach ($for_sortable as $item) {
-        //     $columns_for_insert .= $item;
-        //     $columns_for_insert .= ', ';
-        // }
-        // echo $columns_for_insert;
+        echo $str_q=substr_replace($str_q, ')', -2, -1);
+        
+        $max_column_num = count($for_sortable);
+        $unicode =$this->db->prepare("SET NAMES utf8 COLLATE utf8_unicode_ci");
+        $unicode->execute();
 
-
-
-
-
-
-        $length = count($for_sortable);
-        echo '<br>';
-        global $i;
         for ($i = 2; $i < $totalRows; $i++) {
-            for ($column_num = 0; $column_num < $length; $column_num++) {
+            $str_q_values='VALUES (';
+            for ($column_num = 0; $column_num < $max_column_num; $column_num++) {
                 $columns_value = $obj_php_excel->getActiveSheet()->getCellByColumnAndRow($column_num, $i)->getValue();
-                echo $columns_value . ' ';
+            $str_q_values.='\''.$columns_value.'\', ';
             }
-            echo '<br>';
+            $str_q_values=$str_q_values.')");';
+            $str_q_values=substr_replace($str_q_values, ')")', -6, -1);
         }
-        //$insert_contacts = $this->db->prepare("INSERT INTO `komus_new`.`contacts` ($columns_for_insert) VALUES ('2020-02-12 13:17:35', '21', '29', :col_val)");
+        echo $str_q_values;
+        // $insert_contacts = $this->db->prepare("INSERT INTO `komus_new`.`contacts` (`creation_time`,`regions_id`, `users_id`, $col_name) VALUES ('2020-02-12 13:17:35', '21', '29', :col_val)");
         // $insert_contacts->bindParam(':col_val', $column_value, \PDO::PARAM_STR);
-        //     $alter_table_contacts = $this->db->prepare("ALTER TABLE `contacts` ADD COLUMN 
-        //                         $one_word_column_name VARCHAR(255) NULL DEFAULT NULL AFTER `id`");
-        //     try {
-        //         $alter_table_contacts->execute();
-        //     } catch (\Throwable $th) {
-        //         //TODO :убрать в отдельную функцию  errorReporter
-        //         die('Произошла ошибка при добавлении поля в таблицу contacts '
-        //             . $th->getMessage() . ' в строке № ' . $th->getLine() .
-        //             ' по адресу: ' . $_SERVER['SCRIPT_NAME']);
-        //     }
-        // }
-        // //добавление данных в базу после динамического создания полей
-        // global $column_num;
-        // for ($column_num = 0;; $column_num++) {
-        //     for ($row_num = 1;; $row_num++) {
-        //         $column_value = $obj_php_excel->getActiveSheet()->getCellByColumnAndRow($column_num, $row_num)->getValue();
-        //         if ($column_value != NULL) {
-        //             echo $column_value . '<br>';
-        //         } else {
-        //             break;
-        //         }
-        // $insert_contacts = $db->prepare("INSERT INTO `komus_new`.`contacts` (`creation_time`,`regions_id`, `users_id`, $col_name) VALUES ('2020-02-12 13:17:35', '21', '29', :col_val)");
-        // $insert_contacts->bindParam(':col_val', $column_value, PDO::PARAM_STR);
+        // $insert_contacts->bindParam(':col_val', $col_name, \PDO::PARAM_STR);
         // try {
         //     $insert_contacts->execute();
-        // } catch (\Throwable $th) {
+        // } 
+        // catch (\Throwable $th) {
         //     die('Произошла ошибка при добавлении значения ' . $column_value . ' в таблицу contacts '
-        //         . $th->getMessage() . ' в строке №' . $th->getLine() .
-        //         ' по адресу: ' . $_SERVER['SCRIPT_NAME']);
+        //         . $th->getMessage() . ' в строке №' . $th->getLine() 
+        //         .' по адресу: ' . $_SERVER['SCRIPT_NAME']);
         // }
-        //     }
-        // }
+        //}
+
     }
     /**
      * Read
