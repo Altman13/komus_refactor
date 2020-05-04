7aa0d358ed320fece6632a81b3e161b452dce627
Altman13 <consent71@gmail.com>
2020-05-04 17:04:36 +0300

    form render in process

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -13,44 +13,27 @@ import { AppActions } from "../models/actions";
 import { ThunkDispatch } from "redux-thunk";
 import { Contact } from "../models";
 import { rec_call } from "../actions/";
-import { keys } from "@material-ui/core/styles/createBreakpoints";
-import { constants } from "buffer";
-// interface Props {
-//   classes: any;
-//   //openSession: typeof openSession
-//   // history: any
-//   // location: any
-//   //session: session
-// }
+
+import Box from "@material-ui/core/Box";
 
 interface State {
-  //endpoint: string
-  // username: string;
-  // password: string;
-  // submitted: boolean;
-  // failure: boolean;
-  // persistent: boolean;
-  // adress: string;
   temp: any;
   loading: boolean;
 }
-interface Block {
-  _uid: string;
-  component: string;
-  headline: string;
-}
+
 type Props = LinkStateProps & LinkDispatchProps;
 export class FormComponent extends React.Component<Props, State> {
   _isMounted = false;
+
   constructor(props: Props) {
     super(props);
   }
   // handleChange(e: React.ChangeEvent<HTMLInputElement>) {
   //   const { name, value } = e.target;
   //   switch (name) {
-  //     // case "endpoint":
-  //     //     this.setState({ endpoint: value })
-  //     //     break
+  // case "endpoint":
+  //     this.setState({ endpoint: value })
+  //     break
   //     case "username":
   //       this.setState({ username: value });
   //       break;
@@ -70,162 +53,112 @@ export class FormComponent extends React.Component<Props, State> {
   //   }
   // }
   componentDidMount() {
-    //console.log(this.test)
-    // const response = fetch("http://localhost/komus_new/api/calls")
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //  this.test = data[0]["inn"]
-    // this.setState({temp : data[0]["inn"]})
-    //   })
-    //   console.log(this.test)
-    //console.log(this.state.temp)
-    // this._isMounted = true;
-    // if (this._isMounted) {
-    //   const ff=this.getcall()
-    //   console.log(ff)
-    //console.log(this.state.temp)
-    //}
-
-    //this.getcall()
-    // const response = fetch("http://localhost/komus_new/api/calls")
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     //this.inn = data.inn
-    //   //this.setState({inn : data.inn})
-    //   })
-    //console.log(this.inn)
-    // fetch("http://localhost/komus_new/api/calls")
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     //console.log(json)
-    //     this.setState({data : json})
-    //   })
-    //console.log(this.state)
-    // const cont =this.getcall()
-    //console.log(this.state.data)
-    // this.setState({data : cont})
-    //fetch("http://localhost/komus_new/api/calls")
-    // await fetch("https://jsonplaceholder.typicode.com/todos/1")
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     //  const response = await fetch("http://localhost/komus_new/api/calls")
-    //     //  const json = await response.json();
-    //     //  this.setState({data : json})
-    //     // .then((response) => response.json())
-    //     // .then((json) => {
-    //     //if (this._is_mounted) {
-    //     this.setState({ data: json, loading: true });
-    //     // this.adress = json.adres;
-    //     //}
-    //   });
     this.rec_call();
   }
 
   rec_call = (): void => {
     this.props.receiveCall();
   };
+
   render() {
     const { contacts } = this.props;
-      contacts.map(cont=>{
-        console.log(Object.keys(cont))
-        console.log(Object.values(cont))
-      })
-    // contacts.forEach((element) => {
-    //   for (let index = 0; index < 10; index++) {
-    //     console.log(element[index].name);
-    //   }
-    // });
-
-    //{data.content.body.map(block => console.log(block.component))}
-
-    // contacts[0].map(function(v: Contact, i: number){
-    //   console.log(v.name+ ' ' + i)
-    // })
-    // for (var prop in contacts[0]) {
-    //     console.log(prop)
-    //   }
-    //const template = Object.keys(contacts[0]).map(item => <span key={item.id}>{item.email}</span>)
-    // contacts[0].map(function(v: any, i: number){
-    //     console.log(v.name+ ' ' + i)
-    //   })
-    //const { classes } = this.props;
-    // const _is_mounted = this._is_mounted;
-    // if(this._is_mounted)
+    var obj;
+    Object.keys(contacts).forEach(function eachKey(key) {
+      obj = contacts[key];
+    });
+    console.log(typeof obj);
+    let html_element = new Array();
+    if (obj)
+      for (let [key, value] of Object.entries(obj)) {
+        //console.log(`${key}: ${value}`);
+        if (value)
+          html_element.push(
+            <div id={key} style={{ fontSize: 18 }}>
+              {key}: {value}
+            </div>
+          );
+      }
     return (
-      <Container component="main" maxWidth="sm">
-        <CssBaseline />
-        <div>
-          {/* {this.state.failure && (
-            <React.Fragment>
-              <div className={classes.failure}>Неудачный вход</div>
-              <div className={classes.failure}>Некорректные логин/пароль</div>
-            </React.Fragment>
-          )} */}
-          <form
-            className="form"
-            noValidate
-            // onSubmit={this.handleSubmit.bind(this)}
-          >
-            <InfoTextBlock />
-            
-            {contacts.map((contact) => (
+      <Container component="main">
+        <Box
+          display="flex"
+          justifyContent="center"
+          m={1}
+          p={1}
+          bgcolor="c4e1a5"
+        >
+          <Box p={1} style={{ border: "2px solid" }} width="25%" boxShadow={3}>
+            {html_element}
+          </Box>
+          <Box p={1} bgcolor="#c4e1a5" width="75%" boxShadow={1}>
+            <form className="form" noValidate>
+              <InfoTextBlock />
+              {/* {contacts.map((contact) => (
+              <div>
                 <p id={`${Object.keys(contact)}`}>{Object.values(contact)}</p>
-            ))}
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Наименование организации"
-              name="company_name"
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="fio_lpr"
-              label="ФИО ЛПР"
-              name="fio_lpr"
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="phone"
-              label="телефон организации"
-              name="company_phone"
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="phone"
-              label="почта организации"
-              name="company_mail"
-            />
-            <CustomizedSelects />
-            <MailSendComponent />
-            <TextareaAutosize
-              aria-label="minimum height"
-              rowsMin={3}
-              placeholder="Комментарий оператора"
-              style={{ width: "100%" }}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              className="submit"
-            >
-              Продолжить
-            </Button>
-          </form>
-          <input type="button" value="test" onClick={() => this.rec_call()} />
-        </div>
+                <p>{contact.id}</p>
+                <p>{contact.fio}</p>
+                <p>{contact.email}</p>
+              </div>
+            ))} */}
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Наименование организации"
+                name="company_name"
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="fio_lpr"
+                label="ФИО ЛПР"
+                name="fio_lpr"
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="phone"
+                label="телефон организации"
+                name="company_phone"
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="phone"
+                label="почта организации"
+                name="company_mail"
+              />
+              <CustomizedSelects />
+              <MailSendComponent />
+              <TextareaAutosize
+                aria-label="minimum height"
+                rowsMin={3}
+                placeholder="Комментарий оператора"
+                style={{ width: "100%" }}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className="submit"
+              >
+                Продолжить
+              </Button>
+            </form>
+          </Box>
+          <Box p={1} style={{ border: "2px solid" }} width="25%" boxShadow={3}>
+            Дополнительный блок
+          </Box>
+        </Box>
       </Container>
     );
   }
@@ -237,20 +170,14 @@ interface LinkDispatchProps {
   //makeCall: (contacts: Contact ) => void;
   receiveCall: () => void;
 }
-const mapStateToProps = (
-  state: AppState
-  //ownProps: HomePageProps
-): LinkStateProps => ({
+const mapStateToProps = (state: AppState): LinkStateProps => ({
   contacts: state.contacts,
 });
 
 const mapDispatchToProps = (
   dispatch: ThunkDispatch<any, any, AppActions>
-  //ownProps: HomePageProps
 ): LinkDispatchProps => ({
   receiveCall: bindActionCreators(rec_call, dispatch),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(FormComponent);
-
-//export default FormComponent;
--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { store } from './store';
 
 ReactDOM.render(
 <Provider store = {store}>
-<LoginTemp classes/>
+{/* <LoginTemp classes/> */}
 <FormComponent/>
 </Provider>,
 
