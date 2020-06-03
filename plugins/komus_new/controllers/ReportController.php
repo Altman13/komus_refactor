4e5c43a48648a940eab92615add92732eb9a57de
Altman13 <consent71@gmail.com>
2020-06-03 18:40:35 +0300

    small edits

--- plugins/komus_new/controllers/ReportController.php
+++ plugins/komus_new/controllers/ReportController.php
@@ -16,20 +16,25 @@ class ReportController
     {
         //set_time_limit(1800);
         //TODO: сформировать рамку у отчета и шапку отчета
-        $objPHPExcel = new PHPExcel();
-        $objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
-        $objPHPExcel->setActiveSheetIndex(0);
+        $obj_php_excel = new PHPExcel();
+        $obj_writer = new PHPExcel_Writer_Excel2007($obj_php_excel);
+        $obj_php_excel->setActiveSheetIndex(0);
         $data_for_xls = $this->report->read();
         $row_num = 0;
-            foreach ($data_for_xls as $key => $data_row) {
-                $clm_num = 0;
-                foreach ($data_row as $key => $column_val) {
-                    //echo $column_val . ' ';
-                    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($clm_num, $row_num, $column_val);
+        foreach ($data_for_xls as $key => $data_row) {
+            $clm_num = 0;
+            foreach ($data_row as $key => $column_val) {
+                if ($row_num ==1) {
+                    $obj_php_excel->getActiveSheet()->setCellValueByColumnAndRow($clm_num, $row_num, $key);
                     $clm_num++;
                 }
-                $row_num++;
+                else{
+                    $obj_php_excel->getActiveSheet()->setCellValueByColumnAndRow($clm_num, $row_num, $column_val);
+                    $clm_num++;
+                } 
             }
-        $objWriter->save('report.xlsx');
+            $row_num++;
+        }
+        $obj_writer->save('report.xlsx');
     }
 }
