50faba81b62ca3abdaa767057e8c2ecb8704108d
Altman13 <consent71@gmail.com>
2020-06-03 10:07:28 +0300

    reports in process

--- plugins/komus_new/controllers/ReportController.php
+++ plugins/komus_new/controllers/ReportController.php
@@ -1,17 +1,29 @@
 <?php
 
 use Komus\report;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Container;
 
 class ReportController
 {
-    //private $report;
-    public function __construct()
+    private $report;
+    public function __construct(Container $container)
     {
-        //$this->report=$report;
+        $this->report = $container['report'];
     }
     public function show()
     {
-        echo 'reportsController';
-        //$this->report->Read();
+        $data_for_xls = $this->report->read();
+        $row_count = count($data_for_xls);
+        $k=0;
+        for ($i = 0; $i < $row_count; $i++) {
+            foreach ($data_for_xls as $key => $data_row) {
+                foreach ($data_row as $key => $column_val) {
+                echo $column_val . '<br>';
+                }
+            }
+            //echo $k++;
+        }
     }
 }
