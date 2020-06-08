a59003b09ff2b6b2a8a42d2535b1eaf70a1e4acf
Altman13 <consent71@gmail.com>
2020-06-08 13:45:44 +0300

    small edits

--- src/components/ListOperatorsComponent.tsx
+++ src/components/ListOperatorsComponent.tsx
@@ -1,45 +1,39 @@
 /* eslint-disable no-use-before-define */
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import { Divider } from '@material-ui/core';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { Divider } from "@material-ui/core";
 
-const get_users = (
-    async () => {
-      try {
-        let resp=''
-        await fetch("http://localhost/komus_new/api/user")
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            resp = data
-          });
-        return resp
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  )
+var users = new Array();
 
+// interface oper {
+//   operators: string
+// }
+
+async function get_users() {
+  try {
+    await fetch("http://localhost/komus_new/api/user")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        users = data;
+      });
+  } catch (err) {
+    console.log(err);
+  }
+}
 export default function ListOperators() {
-  get_users()
+  get_users();
   return (
     <Autocomplete
       id="combo-box-demo"
-      options={temp}
-      getOptionLabel={(option) => option.title}
-      style={{ width: 300 }}
-      renderInput={(params) =>
-      <TextField {...params} label="Список операторов" variant="outlined"/>
-      } 
+      options={users}
+      getOptionLabel={(options) => options.operators}
+      style={{ width: 250, height: 100 }}
+      renderInput={(params) => (
+        <TextField {...params} label="Список операторов" variant="outlined" />
+      )}
     />
   );
 }
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const temp = [
-  { title: 'The Shawshank Redemption'},
-  { title: 'The Godfather'},
-  { title: 'The Godfather: Part II'},
-];
