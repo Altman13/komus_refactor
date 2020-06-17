cb2ee80b2776b304ff6f25ca217678cd541c34ba
Altman13 <consent71@gmail.com>
2020-06-17 10:23:17 +0300

    small edits

--- src/components/NoticeComponent.tsx
+++ src/components/NoticeComponent.tsx
@@ -20,7 +20,11 @@ const useStyles = makeStyles((theme: Theme) => ({
 export default function DefaultNotice() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
-
+  const [state, setState] = React.useState({
+    vertical: 'top' as 'top',
+    horizontal: 'center' as 'center',
+  });
+  const { vertical, horizontal } = state;
   const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
     if (reason === "clickaway") {
       return;
@@ -33,7 +37,7 @@ export default function DefaultNotice() {
       {/* <Button variant="outlined" onClick={handleClick}>
         Open success snackbar
       </Button> */}
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{ vertical, horizontal }} key={vertical + horizontal} >
         <Alert onClose={handleClose} severity="success">
           Действие выполнено успешно!
         </Alert>
