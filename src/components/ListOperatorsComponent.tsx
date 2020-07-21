47462e324283a3111a5307f7e44ce651ca288f7c
Altman13 <consent71@gmail.com>
2020-07-21 07:16:11 +0300

    small edits

--- src/components/ListOperatorsComponent.tsx
+++ src/components/ListOperatorsComponent.tsx
@@ -2,33 +2,48 @@ import React from "react"
 import Autocomplete from "@material-ui/lab/Autocomplete"
 import * as core from "@material-ui/core"
 
+import SpinnerComponent from './SpinnerComponent'
+import NoticeModal from './NoticeComponent'
+
 import { ajaxAction } from '../services'
 
 export default function ListOperators( data : any ) {
-  const [oper, setOperator] = React.useState( "" )
+  
   const { users } = data
+  
+  const [oper, setOperator] = React.useState( "" )
+  const [spinner, setSpinnerVisible] = React.useState( false )
+  const [error , setError] = React.useState( false )
+  const [noticeModal, setVisibleNoticeModal] = React.useState( false )
+  
   function ChooseStOperator( operator ){
     setOperator( operator )
   }
 
   async function setStOperator( ) {
-    console.log('test')
     if ( oper ) {
       const url : string = 'user'
       const method : string = 'PATCH'
+      setSpinnerVisible( true )
       const resp : any = await ajaxAction( url, method , oper )
-      return resp
+      if(resp){
+        setSpinnerVisible( false )
+        setVisibleNoticeModal( true )
+      //return resp
+      }
     }
   }
   return (
     <div>
+    { noticeModal ? <NoticeModal/>: null }
+    { spinner ? <SpinnerComponent/> : 
       <core.Grid item xs = { 12 } lg = { 3 } sm = { 4 } md = { 4 }>
       <Autocomplete
         id="operators"
         freeSolo
-        onChange = {( event, value ) => ChooseStOperator( value )}
-        options={ users.map(( option)  => option.operators )}
-        renderInput={(params) => (
+        onChange = { ( event, value ) => ChooseStOperator( value ) }
+        options={ users.map( ( option )  => option.operators ) }
+        renderInput={ ( params ) => (
           <core.TextField {...params} label="Выбрать оператора" margin="normal" variant="outlined" />
         )}
       />
@@ -41,6 +56,7 @@ export default function ListOperators( data : any ) {
         Назначить
       </core.Button>
       </core.Grid>
+    }
     </div>  
   )
 }
