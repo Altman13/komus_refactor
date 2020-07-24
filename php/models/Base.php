84d71a097a68f0ba2cc5fec9bcd62593698d6cfe
Altman13 <consent71@gmail.com>
2020-07-24 07:00:50 +0300

    small edits

--- php/models/Base.php
+++ php/models/Base.php
@@ -19,27 +19,29 @@ class Base
     }
     public function create($files)
     {
+        //TODO : убедиться в том, что не может быть пропущенных пустых столбцов в файле для импорта базы,
+        //!иначе загрузка произойдет до первого пустого столбца заголовка
+
         try {
             $uploadfile = $this->uploadFile($files);
             $total_rows = $this->setSettingsXls($uploadfile);
-            //TODO : убедиться в том, что не может быть пропущенных пустых столбцов в файле для импорта базы,
-            //!иначе загрузка произойдет до первого пустого столбца заголовка
             $columns_name = array();
             $query_insert_columns_name = "INSERT INTO contacts (";
             $data = array();
-            for ($i = 0;; $i++) {
+            $i=0;
+            do {
                 $column_name_rus = $this->obj_php_excel->getActiveSheet()->getCellByColumnAndRow($i, 1)->getValue();
-                if ($column_name_rus != NULL) {
-                    $column_name_temp = $this->translitColumn($column_name_rus);
-                    $column_name_translit = explode('-', $column_name_temp, 2);
-                    $column_name_translit = preg_replace("/[^a-zA-ZА\s]/", '', $column_name_translit[0]);
-                    array_push($columns_name, $column_name_translit);
-                    $query_insert_columns_name .= '`' . $column_name_translit . '`, ';
-                    $data[$column_name_translit] = strval($column_name_rus);
-                    $alter_table_contacts = $this->db->prepare("ALTER TABLE contacts ADD IF NOT EXISTS $column_name_translit VARCHAR(255)");
-                    $alter_table_contacts->execute();
-                }
-            }
+                $column_name_temp = $this->translitColumn($column_name_rus);
+                $column_name_translit = explode('-', $column_name_temp);
+                $column_name_translit = preg_replace("/[^a-zA-ZА\s]/", '', $column_name_translit);
+                array_push($columns_name, $column_name_translit);
+                $query_insert_columns_name .= '`' . $column_name_translit . '`, ';
+                $query_insert_columns_name = substr_replace($query_insert_columns_name, ',`regions_id`, `users_id`)', -2, -1);
+                $data[$column_name_translit] = strval($column_name_rus);
+                $alter_table_contacts = $this->db->prepare("ALTER TABLE contacts ADD IF NOT EXISTS $column_name_translit VARCHAR(255)");
+                $alter_table_contacts->execute();
+                $i++;
+            } while ($column_name_rus != NULL);
             $this->saveArrayToFile($data);
             $this->insertDb($query_insert_columns_name, $total_rows, $columns_name);
         } catch (\Throwable $th) {
@@ -71,32 +73,34 @@ class Base
         $total_rows = $worksheetData[0]['total_rows'];
         return $total_rows;
     }
-    public function saveArrayToFile($data)
-    {
-            $data_json = json_encode($data, JSON_UNESCAPED_UNICODE);
-            $fn = "columns_name.json";
-            file_put_contents($fn, $data_json);    
-    }
     public function translitColumn($column_name_rus)
     {
-        $column_name_rus = (string) $column_name_rus; 
-        $column_name_rus = strip_tags($column_name_rus); 
-        $column_name_rus = str_replace(array("\n", "\r"), " ", $column_name_rus); 
+        $column_name_rus = (string) $column_name_rus;
+        $column_name_rus = strip_tags($column_name_rus);
+        $column_name_rus = str_replace(array("\n", "\r"), " ", $column_name_rus);
         $column_name_rus = preg_replace("/\s+/", ' ', $column_name_rus);
-        $column_name_rus = trim($column_name_rus); 
-        $column_name_rus = function_exists('mb_strtolower') ? mb_strtolower($column_name_rus) : strtolower($column_name_rus); 
-        $column_name_rus = strtr($column_name_rus, array('а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'e', 
-        'ж' => 'j', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 
-        'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch', 
-        'ш' => 'sh', 'щ' => 'shch', 'ы' => 'y', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya', 'ъ' => '', 'ь' => ''));
+        $column_name_rus = trim($column_name_rus);
+        $column_name_rus = function_exists('mb_strtolower') ? mb_strtolower($column_name_rus) : strtolower($column_name_rus);
+        $column_name_rus = strtr($column_name_rus, array(
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'e',
+            'ж' => 'j', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+            'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
+            'ш' => 'sh', 'щ' => 'shch', 'ы' => 'y', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya', 'ъ' => '', 'ь' => ''
+        ));
         // очищаем строку от недопустимых символов
-        $column_name_rus = preg_replace("/[^0-9a-z-_ ]/i", "", $column_name_rus); 
+        $column_name_rus = preg_replace("/[^0-9a-z-_ ]/i", "", $column_name_rus);
         $column_name_translit = str_replace(" ", "-", $column_name_rus);
         return $column_name_translit;
     }
+    public function saveArrayToFile($data)
+    {
+        $data_json = json_encode($data, JSON_UNESCAPED_UNICODE);
+        $fn = "columns_name.json";
+        file_put_contents($fn, $data_json);
+    }
+
     public function insertDb($query_insert_columns_name, $total_rows, $columns_name)
     {
-        $query_insert_columns_name = substr_replace($query_insert_columns_name, ',`regions_id`, `users_id`)', -2, -1);
         for ($i = 1; $i < $total_rows; $i++) {
             $str_q_values = 'VALUES (';
             for ($column_num = 0; $column_num < count($columns_name); $column_num++) {
